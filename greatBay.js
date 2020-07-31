8a32fcb1b773b0f9bb106050f1942b61709cb28f
marasinipradeep <marasinipradeep@gmail.com>
2020-07-31 15:49:22 +0930

    #User can post and bid an item.

--- greatBay.js
+++ greatBay.js
@@ -1,19 +1,25 @@
 const mysql = require("mysql")
 const inquirer = require("inquirer")
 
+
+//Basic database settings
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "alterPassword",
-    database: "greatbay_db"
+    password: "*****",//put your password
+    database: "greatbay_db" //create database using Mysql workbench
 });
 
+
+//connect database
 connection.connect((err) => {
     if (err) throw err;
     console.log("Database connected");
     askQuestion();
 })
+
+//ask user questions on initial start
 
 function askQuestion() {
     inquirer.prompt(
@@ -41,6 +47,8 @@ function askQuestion() {
         })
 }
 
+
+//If user chooses to post an item
 function postItem() {
     inquirer.prompt([{
         message: "What is the name of Item?",
@@ -76,6 +84,8 @@ function postItem() {
         })
 }
 
+
+//If user chooses to bid an item
 function bidItem() {
 
     connection.query("SELECT * FROM auctions", (err, results) => {
@@ -84,7 +94,61 @@ function bidItem() {
 
         //Once you have an item prompt user which item they want
 
+        inquirer.prompt([{
+            name: "choices",
+            type: "rawlist",
+            choices: function () {
+                var choiceArray = [];
+                for (var i = 0; i < results.length; i++) {
+                    choiceArray.push(results[i].item_name);
+                }
+                return choiceArray;
+            },
+            message: "What auction would you like to place a bid in ?"
+        },
+        {
+            name: "bid",
+            type: "input",
+            message: "How much would you like to bid ?"
+        }
+        ])
+            .then((answer) => {
 
+                //get the information of choosen item
+                var choosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_name === answer.choices) {
+                        choosenItem = results[i]
+
+                        console.log("choosen item is " + choosenItem)
+                    }
+                }
+
+                //Determine if bid was high enough
+                if (choosenItem.highest_bid < parseInt(answer.bid)) {
+                    //bid is high so upadte table inform the user and start again
+                    connection.query("UPDATE auctions SET ? WHERE ?",
+
+                        [
+                            {
+                                highest_bid: answer.bid
+                            },
+                            {
+                                id: choosenItem.id
+                            }
+                        ],
+                        function (err) {
+                            if (err) throw err;
+                            console.log("Bid place successfully");
+                            askQuestion();
+                        }
+                    );
+                }
+                else {
+                    //bid was less then original bid
+                    console.log("your bid is less then others bid")
+                    askQuestion()
+                }
+            })
     })
-
 }
